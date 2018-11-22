895c8db0c9df0cd342f40d5ff57220b3698b7246
Marlon <marlon@MacBook-Air-de-Marlon.local>
2018-11-22 18:01:17 -0200

    Método de Requisicao HTTP

--- app/src/main/java/com/example/marcioal1991/garagemclub/FormUsuarioView.java
+++ app/src/main/java/com/example/marcioal1991/garagemclub/FormUsuarioView.java
@@ -2,26 +2,45 @@ package com.example.marcioal1991.garagemclub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class FormUsuarioView extends AppCompatActivity {
 
-    EditText nome;
-    EditText data_nascimento;
-    EditText estado_civil;
-    EditText cpf;
-    EditText cep;
-    EditText cidade;
-    EditText bairro;
-    EditText endereco;
-    EditText telefone;
+    EditText et_nome;
+    EditText et_data_nascimento;
+    EditText et_estado_civil;
+    EditText et_cpf;
+    EditText et_cep;
+    EditText et_cidade;
+    EditText et_bairro;
+    EditText et_endereco;
+    EditText et_telefone;
 
-    RadioButton masculino;
-    RadioButton feminino;
+    RadioButton rb_masculino;
+    RadioButton rb_feminino;
+
+    ImageButton img_foto;
+
+
+    String nome;
+    String data_nascimento;
+    String estado_civil;
+    String cpf;
+    String cep;
+    String cidade;
+    String bairro;
+    String endereco;
+    String telefone;
+
+    String masculino;
+    String feminino;
 
     ImageButton foto;
+
 
 
     @Override
@@ -29,22 +48,34 @@ public class FormUsuarioView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_usuario_view);
 
-        nome = (EditText) findViewById(R.id.et_nome);
-        data_nascimento = (EditText) findViewById(R.id.et_data_nascimento);
-        estado_civil = (EditText) findViewById(R.id.et_estado_civil);
-        cpf = (EditText) findViewById(R.id.et_cpf);
-        cep = (EditText) findViewById(R.id.et_cep);
-        cidade = (EditText) findViewById(R.id.et_cidade);
-        bairro = (EditText) findViewById(R.id.et_bairro);
-        endereco = (EditText) findViewById(R.id.et_endereco);
-        telefone = (EditText) findViewById(R.id.et_telefone);
+        et_nome = (EditText) findViewById(R.id.et_nome);
+        et_data_nascimento = (EditText) findViewById(R.id.et_data_nascimento);
+        et_estado_civil = (EditText) findViewById(R.id.et_estado_civil);
+        et_cpf = (EditText) findViewById(R.id.et_cpf);
+        et_cep = (EditText) findViewById(R.id.et_cep);
+        et_cidade = (EditText) findViewById(R.id.et_cidade);
+        et_bairro = (EditText) findViewById(R.id.et_bairro);
+        et_endereco = (EditText) findViewById(R.id.et_endereco);
+        et_telefone = (EditText) findViewById(R.id.et_telefone);
 
-        masculino = (RadioButton) findViewById(R.id.rb_masculino);
-        feminino = (RadioButton) findViewById(R.id.rb_feminino);
+        rb_masculino = (RadioButton) findViewById(R.id.rb_masculino);
+        rb_feminino = (RadioButton) findViewById(R.id.rb_feminino);
 
-        foto = (ImageButton) findViewById(R.id.ib_foto);
+        img_foto = (ImageButton) findViewById(R.id.ib_foto);
     }
 
+    public void submitForm(View view) {
 
-
+        nome = et_nome.getText().toString();
+        data_nascimento = et_data_nascimento.getText().toString();
+        estado_civil = et_estado_civil.getText().toString();
+        cpf = et_cpf.getText().toString();
+        cep = et_cep.getText().toString();
+        cidade = et_cidade.getText().toString();
+        bairro = et_bairro.getText().toString();
+        endereco = et_endereco.getText().toString();
+        telefone = et_telefone.getText().toString();
+        masculino = rb_masculino.getText().toString();
+        feminino = rb_feminino.getText().toString();
+    }
 }
