35ed9a0ef82c61e32d0bc933b42ea2656ba5f044
Marcio A. de Lima <marcioal1991@gmail.com>
2018-12-03 19:11:50 -0200

    Class que cria as tabelas do banco

--- app/src/main/java/com/example/marcioal1991/garagemclub/Banco.java
+++ app/src/main/java/com/example/marcioal1991/garagemclub/Banco.java
@@ -1,4 +1,63 @@
 package com.example.marcioal1991.garagemclub;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class Banco {
+    Context ctx = null;
+    private SQLiteDatabase db;
+
+    public Banco(Context ctx) {
+        this.ctx = ctx;
+        this.db = this.ctx.openOrCreateDatabase("garagem_club", Context.MODE_PRIVATE);
+
+        this.db.execSQL(
+                "CREATE TABLE IF NOT EXISTS garagem_club.usuarios (\n" +
+                "  id INTEGER NOT NULL PRIMARY KEY,\n" +
+                "  login TEXT NOT NULL,\n" +
+                "  senha TEXT NOT NULL,\n" +
+                "  email TEXT NOT NULL);"
+        );
+
+        this.db.execSQL(
+                "CREATE TABLE IF NOT EXISTS pessoas (\n" +
+                        "  id INTEGER NOT NULL PRIMARY KEY,\n" +
+                        "  usuario_id INTEGER NOT NULL,\n" +
+                        "  nome TEXT NOT NULL,\n" +
+                        "  sexo TEXT NOT NULL,\n" +
+                        "  data_nascimento TEXT NOT NULL,\n" +
+                        "  estado_civil TEXT NOT NULL,\n" +
+                        "  cliente INTEGER NOT NULL DEFAULT 0,\n" +
+                        "  proprietario INTEGER NOT NULL DEFAULT 0,\n" +
+                        "  rg TEXT NOT NULL,\n" +
+                        "  cpf TEXT NOT NULL,\n" +
+                        "  endereco TEXT NOT NULL,\n" +
+                        "  cidade TEXT NOT NULL,\n" +
+                        "  bairro TEXT NOT NULL,\n" +
+                        "  estado TEXT NOT NULL DEFAULT 'RS',\n" +
+                        "  cep TEXT NOT NULL,\n" +
+                        "  telefone_fixo TEXT NOT NULL,\n" +
+                        "  telefone_celular TEXT NOT NULL,\n" +
+                        "  foto TEXT NOT NULL);"
+        );
+
+        this.db.execSQL(
+                "CREATE TABLE IF NOT EXISTS garagem_club.`garagens` (\n" +
+                        "    id INTEGER NOT NULL PRIMARY KEY,\n" +
+                        "    proprietario_id INTEGER NOT NULL,\n" +
+                        "    cliente_id INTEGER NOT NULL,\n" +
+                        "    endereco TEXT NOT NULL,\n" +
+                        "    cidade TEXT NOT NULL,\n" +
+                        "    bairro TEXT NOT NULL,\n" +
+                        "    estado TEXT NOT NULL,\n" +
+                        "    cep TEXT NOT NULL,\n" +
+                        "    tamanho INTEGER NOT NULL,\n" +
+                        "    descricao TEXT NOT NULL,\n" +
+                        "    observacao TEXT NOT NULL,\n" +
+                        "    foto TEXT NOT NULL,\n" +
+                        "    valor INTEGER NOT NULL);"
+        );
+
+    }
+
 }
