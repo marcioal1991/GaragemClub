fa4698e8cadc7453c37233654e16f2251d8dcf2a
verardo <vb.verardo@gmail.com>
2018-12-03 23:08:42 -0200

    Alteracoes interface de login

--- app/src/main/java/com/example/marcioal1991/garagemclub/MainActivity.java
+++ app/src/main/java/com/example/marcioal1991/garagemclub/MainActivity.java
@@ -4,33 +4,59 @@ import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText login;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (EditText) findViewById(R.id.login);
+        password = (EditText) findViewById(R.id.password);
     }
 
 
     public void singIn(View view) {
         Banco con = new Banco(this);
 
-        Cursor linhas = con.db.rawQuery("SELECT * FROM usuarios", null);
+        if (login.getText().toString().equals("")) {
+            String texto = "Insira o login";
+            int duracao = Toast.LENGTH_SHORT;
 
-        String retorno = "";
-        if(linhas.moveToFirst()){ //retorna false se não há linhas na tabela
-            do{
-                String ID = linhas.getString(0);
-                String NOME = linhas.getString(1);
-                String PLACA = linhas.getString(2);
-                String ANO = linhas.getString(3);
-                retorno+=ID+","+ NOME+","+PLACA+","+ANO+"\n";
-            }
-            while(linhas.moveToNext()); //laço até a última linha da tabela
+            Toast toast = Toast.makeText(this, texto,duracao);
+            toast.show();
+            return;
         }
-        System.out.println(retorno);
+
+        if (password.getText().toString().equals("")) {
+            String texto = "Insira a senha";
+            int duracao = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(this, texto,duracao);
+            toast.show();
+            return;
+        }
+
+        Cursor linhas = con.db.rawQuery("SELECT count(*) as total FROM usuarios WHERE login = '" + login.getText().toString() + "' AND senha = '" + password.getText().toString() + "'"
+                , null);
+
+        if(linhas.moveToFirst()) { //retorna false se não há linhas na tabela
+            if ( linhas.getInt(0) > 0) {
+                System.out.println("teste passou");
+            } else  {
+                String texto = "Login ou senha inválidos";
+                int duracao = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(this, texto,duracao);
+                toast.show();
+            }
+        }
+
         con.db.close();
 
     }
@@ -38,25 +64,21 @@ public class MainActivity extends AppCompatActivity {
     public void singUp(View view) {
         Banco con = new Banco(this);
         System.out.println("teste");
-        Cursor insert = con.db.rawQuery("insert into usuarios (login,senha,email) values ('teste','1234','teste@teste.com')", null);
+        //Cursor insert = con.db.rawQuery("insert into usuarios (login,senha,email) values ('teste','1234','teste@teste.com')", null);
         Cursor linhas = con.db.rawQuery("SELECT * FROM usuarios", null);
         System.out.println("teste2");
         String retorno = "";
-        System.out.println(linhas.getColumnName(0));
         if(linhas.moveToFirst()){ //retorna false se não há linhas na tabela
             do{
-                //String ID = linhas.getString(0);
-                //String NOME = linhas.getString(1);
-                //String PLACA = linhas.getString(2);
-                //String ANO = linhas.getString(3);
-                //retorno+=ID+","+ NOME+","+PLACA+","+ANO+"\n";
-                System.out.println(linhas.getString(0));
-                System.out.println(linhas.getString(1));
-                System.out.println(linhas.getString(2));
+                String ID = linhas.getString(0);
+                String LOGIN = linhas.getString(1);
+                String SENHA = linhas.getString(2);
+                String EMAIL = linhas.getString(3);
+                retorno +=ID+","+ LOGIN+","+SENHA+","+EMAIL+"\n";
+                System.out.println(retorno);
             }
             while(linhas.moveToNext()); //laço até a última linha da tabela
         }
-        System.out.println(retorno);
         con.db.close();
     }
 }
